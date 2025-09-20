31c8d07e3f4cc1d9449559e4d235867bdc37bae4
loslot <loslot100604@gmail.com>
2025-09-20 12:53:16 -0800

    update

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-12 col-md-6 col-lg-3 p-3">
                 <div class="card p-2">
-                    <img src="/2.png" alt="">
+                    <img src="/3.png" alt="">
                     <div>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita adipisci totam ipsam omnis? Reprehenderit cum ex asperiores libero assumenda quas.
                     </div>
@@ -12,7 +12,7 @@
 
              <div class="col-12 col-md-6 col-lg-3 p-3">
                 <div class="card p-2">
-                    <img src="/2.png" alt="">
+                    <img src="/4.png" alt="">
                     <div>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita adipisci totam ipsam omnis? Reprehenderit cum ex asperiores libero assumenda quas.
                     </div>
@@ -21,7 +21,7 @@
 
              <div class="col-12 col-md-6 col-lg-3 p-3">
                 <div class="card p-2">
-                    <img src="/2.png" alt="">
+                    <img src="/5.png" alt="">
                     <div>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita adipisci totam ipsam omnis? Reprehenderit cum ex asperiores libero assumenda quas.
                     </div>
@@ -30,7 +30,7 @@
 
              <div class="col-12 col-md-6 col-lg-3 p-3">
                 <div class="card p-2">
-                    <img src="/2.png" alt="">
+                    <img src="/6.png" alt="">
                     <div>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita adipisci totam ipsam omnis? Reprehenderit cum ex asperiores libero assumenda quas.
                     </div>
@@ -39,7 +39,7 @@
 
              <div class="col-12 col-md-6 col-lg-3 p-3">
                 <div class="card p-2">
-                    <img src="/2.png" alt="">
+                    <img src="/7.png" alt="">
                     <div>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita adipisci totam ipsam omnis? Reprehenderit cum ex asperiores libero assumenda quas.
                     </div>
@@ -48,7 +48,7 @@
 
              <div class="col-12 col-md-6 col-lg-3 p-3">
                 <div class="card p-2">
-                    <img src="/2.png" alt="">
+                    <img src="/8.png" alt="">
                     <div>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita adipisci totam ipsam omnis? Reprehenderit cum ex asperiores libero assumenda quas.
                     </div>
@@ -57,16 +57,16 @@
 
              <div class="col-12 col-md-6 col-lg-3 p-3">
                 <div class="card p-2">
-                    <img src="/2.png" alt="">
+                    <img src="/9.png" alt="">
                     <div>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita adipisci totam ipsam omnis? Reprehenderit cum ex asperiores libero assumenda quas.
                     </div>
                 </div>
             </div>
 
-             <div class="col-12 col-md-6 col-lg-3 p-3">
+              <div class="col-12 col-md-6 col-lg-3 p-3">
                 <div class="card p-2">
-                    <img src="/2.png" alt="">
+                    <img src="/10.png" alt="">
                     <div>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita adipisci totam ipsam omnis? Reprehenderit cum ex asperiores libero assumenda quas.
                     </div>
